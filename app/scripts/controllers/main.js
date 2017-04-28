63cd7d3c50eae133b064c130c56b7618c5abcd40
Christina <stylistkris@gmail.com>
2017-04-28 14:32:35 +0200

    a lot of work

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beautyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the beautyApp
+ */
+angular.module('beautyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
